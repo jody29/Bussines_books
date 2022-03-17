8911034c87cd07a3ce4a18507a8836d40ab5c551
Jody Lorist <66092262+jody29@users.noreply.github.com>
2022-03-17 15:16:22 +0100

    changed render

--- src/js/modules/render.js
+++ src/js/modules/render.js
@@ -3,10 +3,8 @@ const display = document.querySelector('main ul')
 const render = (data, output, query) => {
     const books = data.results // get the results
     const articles = output.results
-    const all = books + articles
     const results = books.length + articles.length
     const loader = document.querySelectorAll('.loading') // select loaderr
-    console.log(all)
 
     display.insertAdjacentHTML('beforebegin', `<p>${results} resultaten voor: <strong>${query}</strong></p>`) // show how many results
 
@@ -14,11 +12,11 @@ const render = (data, output, query) => {
         load.style = 'display: none;'
     })
 
-    all.forEach((item) => { // for each item in the results list, make an article element
+    books.forEach((item) => { // for each item in the results list, make an article element
         const html = ` 
                 <article class='book'>
                     <img src="${
-                    item.coverimages || typeof item.coverimages !== 'undefined' ? item.coverimages[1] : "https://v112.nbc.bibliotheek.nl/thumbnail?uri=http://data.bibliotheek.nl/ggc/ppn/040228304&token=c1322402" // picture of the book
+                    item.coverimages[1] || typeof item.coverimages[1] !== 'undefined' ? item.coverimages[1] : "https://v112.nbc.bibliotheek.nl/thumbnail?uri=http://data.bibliotheek.nl/ggc/ppn/040228304&token=c1322402" // picture of the book
                     }">
                     <section>
                     <h3>${item.titles[0]}</h3>
